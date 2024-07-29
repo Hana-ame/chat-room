e27a682c4ea788bc252344d9efdb9bcf6aa33b68
moonchan-xyz <nanaka@moonchan.xyz>
2024-07-29 19:28:19 +0800

    动态清零

--- ui/src/views/Test.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <c-mkd text="namae">123123</c-mkd>
-  <n-test></n-test>
-</template>
-
-<script setup lang="ts">
-import NTest from '@/components/NTest.vue'
-import CMkd from '@/components/CMkd';
-</script>
--- ui/upload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd dist
-tar -czvf - assets vite.svg | /c/bin/Nmap/ncat.exe 45.130.22.56 1324
