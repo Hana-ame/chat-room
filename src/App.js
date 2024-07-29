c099a73451b011500e649f46b1d8c204814beb49
moonchan-xyz <nanaka@moonchan.xyz>
2024-07-29 21:22:38 +0800

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Welcome to My React App with Tailwind CSS
+      </h1>
+      <p className="mt-4 text-gray-600">
+        This is a paragraph styled with Tailwind CSS.
+      </p>
     </div>
   );
 }
