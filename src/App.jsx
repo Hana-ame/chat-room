2db926562f16c8b72fd5f83d61ea35f618f5c06a
moonchan-xyz <nanaka@moonchan.xyz>
2024-10-10 05:41:17 +0800

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,12 @@
-import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './layouts/Layout';
-import Profile from './pages/Profile';
-import About from './pages/About';
-import Page404 from './components/Page404';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import PrivateRoute from './routes/PrivateRoute';
-import Postman from './Tools/Postman';
-import PostStatus from './pages/PostStatus';
-import StatusList from './components/StatusList';
+import './App.css'
+import ItemList from './Tools/ItemList';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <About />
-      },
-      {
-        path: 'login',
-        element: <Login />
-      },
-      {
-        path: 'register',
-        element: <Register />
-      },
-      {
-        path: 'profile',
-        element: <PrivateRoute><Profile /></PrivateRoute>
-      },
-      {
-        path: 'post',
-        element: <PrivateRoute><PostStatus /></PrivateRoute>
-      },
-      {
-        path: 'statuses',
-        element: <StatusList></StatusList>
-      },
-      {
-        path: 'postman',
-        element: <Postman />
-      },
-      {
-        path: '*',
-        element: <Page404 />
-      }
-    ]
-  }
-]);
-
-const App = () => {
-  return <RouterProvider router={router} />;
-};
+function App() {
+    return (
+        <div>
+            <ItemList />
+        </div>
+    );
+}
 
 export default App;
